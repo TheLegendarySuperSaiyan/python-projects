6dfb2c58263b3a1249fd4f2a7b39e31cd46aad46
TheLegendarySuperSaiyan <117506663+TheLegendarySuperSaiyan@users.noreply.github.com>
2022-11-06 12:25:52 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+n1 = int(input("Enter Your Number"))
+n2 = int(input("Enter Your Number"))
+
+n3 = n1 + n2
+
+print(n3)
+print(n1 / n2)
+print(n1 * n2)
+print(n1 - n2)
---
+++ string.py
@@ -0,0 +1,3 @@
+n1 = input("Enter The string")
+n2 = input("Enter the string")
+print(n1+n2)
